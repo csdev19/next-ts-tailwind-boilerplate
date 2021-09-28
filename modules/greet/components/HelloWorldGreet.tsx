85f15df6b506b49298869bbe112cdc4486e5561d
cristiandev <cristiansotomayor.dev@gmail.com>
2021-09-27 19:26:19 -0500

    hello world from modules folder

---
+++ modules/greet/components/HelloWorldGreet.tsx
@@ -0,0 +1,7 @@
+import React, { VoidFunctionComponent } from 'react';
+
+const HelloWorldGreet: VoidFunctionComponent = () => {
+  return <div>Hello World</div>;
+};
+
+export default HelloWorldGreet;
